a7736154843876d920c8e54b8a9efd9683b03fca
sb1705 <luna951@hotmail.it>
2016-07-13 19:39:41 +0200

    Create cpyFiletoAnother.c

---
+++ Utilities/cpyFiletoAnother.c
@@ -0,0 +1,31 @@
+//Program to copy the contents of one file into another using fgetc and fputc function
+
+#include<stdio.h>
+#include<process.h>
+ 
+void main() {
+   FILE *fp1, *fp2;
+   char a;
+   clrscr();
+ 
+   fp1 = fopen("test.txt", "r");
+   if (fp1 == NULL) {
+      puts("cannot open this file");
+      exit(1);
+   }
+ 
+   fp2 = fopen("test1.txt", "w");
+   if (fp2 == NULL) {
+      puts("Not able to open this file");
+      fclose(fp1);
+      exit(1);
+   }
+ 
+   do {
+      a = fgetc(fp1);
+      fputc(a, fp2);
+   } while (a != EOF);
+ 
+   fcloseall();
+   getch();
+}
